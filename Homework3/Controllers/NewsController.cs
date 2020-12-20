8877b97c1451a0acf7e446f9112ac17ae412da82
unknown <zlazakovich@gmail.com>
2020-12-20 22:34:05 +0200

    first commit

---
+++ Homework3/Controllers/NewsController.cs
@@ -0,0 +1,24 @@
+﻿using Homework3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework3.Controllers
+{
+    public class NewsController : Controller
+    {
+        private INewsRepository _newsRepository { get; }
+        public NewsController(INewsRepository newsRepository)
+        {
+            _newsRepository = newsRepository;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.News = _newsRepository.GetNews();
+            //ViewBag.News = _newsRepository.GetNews().Count;
+            return View();
+        }
+    }
+}
